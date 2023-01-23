4c2ae7840e1497d12a7135d6612fa44e82c78724
max <maxzhang1119@gmail.com>
2023-01-22 20:52:08 -0500

    got rid of README, added technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact} from 'react-icons/di';
+import { GiBrain} from 'react-icons/gi';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
   <div>
-    Technologies
+    <Section id="tech">
+      <SectionDivider />
+      <br />
+      <SectionTitle>
+        Technologies
+      </SectionTitle>
+      <SectionText>
+        I've worked with a wide range of technologies over my coding journey, here is a list of the ones I am most proficient in. Just know that I am constantly working to learn more and expand this list!
+      </SectionText>
+      <List>
+        <ListItem>
+          <DiReact size="4rem"/>
+          <ListContainer>
+            <ListTitle>Front End</ListTitle>
+            <ListParagraph>
+              Expirience with: <br />
+              React.js
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <DiFirebase size="4rem" />
+          <ListContainer>
+            <ListTitle>Back End</ListTitle>
+            <ListParagraph>
+              Expirience with: <br />
+              Node.js and mySQL
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+        <ListItem>
+          <GiBrain size="4rem" />
+          <ListContainer>
+            <ListTitle>Machine Learning</ListTitle>
+            <ListParagraph>
+              Expirience with: <br />
+              PyTorch and TensorFlow
+            </ListParagraph>
+          </ListContainer>
+        </ListItem>
+      </List>
+    </Section>
   </div>
 );
 
